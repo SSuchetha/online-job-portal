59292fccd9356d26faec929bddf506a338536e24
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2022-01-14 16:24:19 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Path
 from pydantic import BaseModel
+from typing import Optional 
 
 app=FastAPI()
 
@@ -10,7 +11,6 @@ mydb=mysql.connector.connect(user='root' , host='localhost' , database='job_port
 mycursor=mydb.cursor()
 
 class details(BaseModel):
-	uid:int
 	name:str
 	dob:str
 	age:str
@@ -21,14 +21,25 @@ class details(BaseModel):
 	address:str
 	gender:str
 
+class updateDetails(BaseModel):
+	name : Optional[str]=None
+	dob : Optional[str]=None
+	age : Optional[str]=None
+	skills : Optional[str]=None
+	education : Optional[str]=None
+	contact : Optional[str]=None
+	experience : Optional[str]=None
+	address : Optional[str]=None
+	gender : Optional[str]=None
+
 @app.get("/")
 def welcome():
 	return {"Welcome!!"}
 
-@app.get("/read-db-one")
-def read_one():												
+@app.get("/read-db-one/{uid}")
+def read_one(uid: int=Path(None , description="ID", gt=0)):												
 	data=""
-	data=all.readone(mydb,mycursor)
+	data=all.readone(mydb,mycursor,uid)
 	return data
 
 @app.get("/read-db")
@@ -37,20 +48,20 @@ def read_all():
 	data=all.read(mydb,mycursor)
 	return data
 
-@app.post("/insert-data")
-def insert_db():
+@app.post("/insert-data/{uid}")
+def insert_db(uid: int , info:details):
 	data=""
-	data=all.insert(mydb,mycursor)
+	data=all.insert(mydb,mycursor,info,uid)
 	return data
 
-@app.put("/update-db")
-def update_db():
+@app.put("/update-db/{uid}")
+def update_db(uid: int , info:updateDetails):
 	data=""
-	data=all.update(mydb,mycursor)
+	data=all.update(mydb,mycursor,info)
 	return data
 
-@app.delete("/delete-one")
-def delete_db():
+@app.delete("/delete-one/{uid}")
+def delete_db(uid: int=Path(None , description="ID", gt=0)):
 	data=""
-	data=all.delete(mydb,mycursor)
+	data=all.delete(mydb,mycursor,info,uid)
 	return data
