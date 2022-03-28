ce3d7a55a7147b193ee2e79be66d292f7e13997e
nsshayan <nsshayan89@gmail.com>
2022-03-28 16:00:45 +0530

    backend code

---
+++ backend/db/session.py
@@ -0,0 +1,19 @@
+from email.generator import Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+from typing import Generator
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+def get_db() -> Generator:
+    try:
+        db = sessionLocal()
+        yield db
+    finally :
+        db.close()
