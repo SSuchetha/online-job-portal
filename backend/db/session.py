ba95f189cb3a82260f5d66962f238354e974e052
nsshayan <nsshayan89@gmail.com>
2022-03-29 21:50:54 +0530

    front end not working

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,19 +1,38 @@
-from email.generator import Generator
+from typing import Generator
+
+from core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from core.config import settings
-from typing import Generator
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if settings.USE_SQLITE_DB == "True":
+	SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+	engine = create_engine(
+		SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+	)
+else:
+	SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+	engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+
+
+# if you don't want to install postgres or any database, use sqlite, a file system based database,
+# uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db() -> Generator:
     try:
-        db = sessionLocal()
+        db = SessionLocal()
         yield db
-    finally :
+    finally:
         db.close()
